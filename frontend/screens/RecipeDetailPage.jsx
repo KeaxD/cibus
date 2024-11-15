1de1e651c149e1812eae94aa049ba08f0b076827
KeaxD <keanuph@gmail.com>
2024-11-15 11:15:55 -0800

    update packages, ignore deprecation logs for react-native-render-html package

--- frontend/screens/RecipeDetailPage.jsx
+++ frontend/screens/RecipeDetailPage.jsx
@@ -3,10 +3,14 @@ import React from "react";
 import { View, Text, Image, Dimensions, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import RenderHtml from "react-native-render-html";
+import { LogBox } from "react-native";
 
 import styles from "../styles/recipedetailspage";
 
 const RecipeDetailPage = ({ route }) => {
+  //Ignore deprecation messages
+  LogBox.ignoreLogs([/Support for defaultProps will be removed/]);
+
   const { recipe } = route.params;
   const { width } = Dimensions.get("window");
 
@@ -24,6 +28,10 @@ const RecipeDetailPage = ({ route }) => {
   // Function to format ingredient list
   const formatIngredients = (ingredients) => {
     return ingredients.map((ingredient) => ingredient.name).join(", ");
+  };
+
+  const testInstructions = {
+    html: recipe.instructions,
   };
 
   const formattedInstructions = formatInstructions(recipe.instructions);
